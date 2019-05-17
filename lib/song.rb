4ff7529e9dc52b2ede78b2c949ec5ef689b1cc65
Jenna Ransom <jenna.ransom@gmail.com>
2019-05-17 03:36:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,12 +34,8 @@ class Song
   end 
   
   def self.find_or_create_by_name(title) 
-    result = self.all.find_by_name(title)
-     if result 
-       result
-     else 
-      self.create_by_name(title)
-    end
+    self.find_by_name(title) || self.create_by_name(title)
+   
   end 
   
   
