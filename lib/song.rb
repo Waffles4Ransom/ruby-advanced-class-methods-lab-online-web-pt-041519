854f928b54b4e196c8c85f5efca2ac60d034af74
Jenna Ransom <jenna.ransom@gmail.com>
2019-05-17 04:00:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,8 +45,8 @@ class Song
     #parse the data
     song = filename.split("- ")[1].chomp(".mp3")
     data = song.split(",")
-    title = data[1]
-    artist = data[0]
+    name = data[0]
+    artist = data[1]
     #make new instance with above data
     new_song = self.new 
     new_song.name = title
