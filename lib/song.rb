20ede93008f0e7f5cca345e5aa2529a31677c306
Jenna Ransom <jenna.ransom@gmail.com>
2019-05-17 03:37:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,10 +35,11 @@ class Song
   
   def self.find_or_create_by_name(title) 
     self.find_by_name(title) || self.create_by_name(title)
-   
   end 
   
-  
+  def self.alphabetcial
+    self.all.sort_by
+  end 
   
   
 end
